c0864cdeaedf090d52692a67bfe613cc0de299e7
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2025-12-02 12:12:20 +0530

    Add files via upload

---
+++ ATCC/Lab-1/Lab_1_2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    FILE *fp1,*fp2;
+    char filename1[50];
+    char filename2[50];
+    char ch;
+    printf("Enter the name of first file : ");
+    scanf("%s",filename1);
+    printf("Enter name of file 2 : ");
+    scanf("%s",filename2);
+    
+    return 0;
+}

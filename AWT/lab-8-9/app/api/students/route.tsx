3bef4ba46da9c4827d7a93e73b3e6620b9fff744
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2026-01-27 11:10:20 +0530

    Add files via upload

---
+++ AWT/lab-8-9/app/api/students/route.tsx
@@ -0,0 +1,8 @@
+export async function GET(){
+    //return "Hello World"
+    return Response.json({name:'arjun',age:123})
+}
+export async function POST(){
+    //return "Hello World"
+    return Response.json({name:'arjun',age:123})
+}

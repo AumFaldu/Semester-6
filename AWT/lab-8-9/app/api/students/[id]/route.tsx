2466dc19638ab38f58dd26b7fc0147ba3376143c
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2026-02-03 10:17:32 +0530

    Delete AWT/lab-8-9/app directory

--- AWT/lab-8-9/app/api/students/[id]/route.tsx
+++
@@ -1,7 +0,0 @@
-import {NextRequest} from "next/server"
-
-export async function GET(_req:NextRequest,{params}:{params:{id:string}}){
-    //return "Hello World"
-    console.log(params)
-    return Response.json({name:'arjun',age:123})
-}

f9ecaacef1e7c9d8c297a6257ff4c69f81eaea13
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2026-03-12 11:10:36 +0530

    Add files via upload

---
+++ AWT/lab-26/src/App.js
@@ -0,0 +1,10 @@
+  import './App.css';
+  import { Provider } from 'react-redux';
+  import { store } from './store';
+  function App() {
+    return (
+      <Provider store={store}><App /></Provider>
+    );
+  }
+
+  export default App;
